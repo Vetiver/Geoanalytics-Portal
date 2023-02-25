31a2e3d4274448126ec631941f47a267e1d98b21
theverybusybee <v_antonchik@mail.ru>
2023-02-25 18:52:45 +0300

    transparent buttons added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,11 @@ import styles from "./App.module.css";
 import Button from "../ui/button/button";
 
 const App: FC = () => {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <Button content="Демо-доступ" type='transparent' direction="right"></Button>
+    </div>
+  );
 };
 
 export default App;
--- src/components/ui/button/button.jsx
+++ src/components/ui/button/button.jsx
@@ -1,14 +1,56 @@
+import { useState } from "react";
 import styles from "./button.module.css";
 import arrowRight from "../../../images/logos/arrow-right.svg";
 import arrowLeft from "../../../images/logos/arrow-left.svg";
+import gradientArrowRight from "../../../images/logos/gradient-right-arrow.svg";
+
+// type = "default" - кнопка с градиентом | "transparent" - прозрачная кнопка
+
+// direction = "right" - стрелка вправо | "left" - стрелка влево
 
 function Button({ content, type = "default", direction = "right" }) {
+  const [isClick, setIsClick] = useState(false);
+  console.log(isClick);
+
+  const handleOnMouseDown = () => {
+    setIsClick(true);
+  };
+
+  const handleOnMouseUp = () => {
+    setIsClick(false);
+  };
+
   return (
-    <button className={`${styles.button} ${styles[type]}`}>
+    <button
+      className={`${styles.button} ${styles[type]}`}
+      onMouseUp={handleOnMouseUp}
+      onMouseDown={handleOnMouseDown}
+    >
       <div className={styles.contentContainer}>
-        {direction === "left" && <img src={arrowLeft} alt="left arrow" />}
-        <p className={styles.content}>{content}</p>
-        {direction === "right" && <img src={arrowRight} alt="right arrow" />}
+        {direction === "right" ? (
+          <>
+            <p className={styles.content}>{content}</p>
+
+            {isClick ? (
+              <img
+                className={styles.arrow}
+                src={gradientArrowRight}
+                alt="right arrow"
+              />
+            ) : (
+              <img
+                className={styles.arrow}
+                src={arrowRight}
+                alt="right arrow"
+              />
+            )}
+          </>
+        ) : (
+          <>
+            <img className={styles.arrow} src={arrowLeft} alt="left arrow" />
+            <p className={styles.content}>{content}</p>
+          </>
+        )}
       </div>
     </button>
   );
