0416682885eff231724d9cbd5a4b6724e0d165d1
Vetiver <99443703+Vetiver@users.noreply.github.com>
2023-02-28 20:09:42 +0300

    jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import styles from "./App.module.css";
 import MainPage from "../pages/main-page/main-page";
 
-const App: FC = () => {
+const App = () => {
   return (
     <div className={styles.page}>
       <MainPage />
