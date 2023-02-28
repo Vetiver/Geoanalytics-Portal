509502b9d5daa5004a3efa615dd304ff2d767c18
Vetiver <99443703+Vetiver@users.noreply.github.com>
2023-02-28 20:08:37 +0300

    комит

--- src/components/App/App.jsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import styles from "./App.module.css";
-import GeodeticApplicationPage from "../../pages/GeodeticApplicationPage/GeodeticApplicationPage";
-
-const App = () => {
-  return <GeodeticApplicationPage />;
-};
-
-export default App;
---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import styles from "./App.module.css";
+import MainPage from "../pages/main-page/main-page";
+
+const App: FC = () => {
+  return (
+    <div className={styles.page}>
+      <MainPage />
+    </div>
+  );
+};
