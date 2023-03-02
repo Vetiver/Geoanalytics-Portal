41b98972121dc3b095a6f82f0a94b015cc800466
theverybusybee <v_antonchik@mail.ru>
2023-03-02 16:29:44 +0300

    background fixed, gradients added

--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -14,57 +14,58 @@ import { Link } from "react-router-dom";
 function MainPage() {
   return (
     <div className={styles.mainPage}>
-      <Header />
-      <main className={styles.main}>
-        <section className={styles.introduction}>
-          <div className={styles.blockWithLink}>
-            <h1 className={styles.heading}>GeoAnalytics.ai</h1>
-            <p className={styles.caption}>
-              Платформа для эффективного управления территорией на основе
-              искусственного интеллекта
-            </p>
-            <Link to='/application'>
-              <Button content="Демо-доступ" />
-            </Link>
-          </div>
-        </section>
-        <section className={styles.keyOpportunities}>
-          <div className={styles.keyOpportunitiesContainer}>
-            <h2 className={styles.keyOpportunitiesTitle}>
-              Ключевые возможности
-            </h2>
-            <div className={styles.cardsContainer}>
-              {keyOpportunityCards.map((card) => {
-                return (
-                  <OpportunityCard
-                    image={card.image}
-                    imageCaption={card.title}
-                    title={card.title}
-                    content={card.caption}
-                  />
-                );
-              })}
-              <img
-                className={`${styles.line} ${styles.cornerLineLeft}`}
-                src={cornerLineLeft}
-                alt="Декоративный элемент"
-              />
-              <img
-                className={`${styles.line} ${styles.straightLine}`}
-                src={straightLine}
-                alt="Декоративный элемент"
-              />
-              <img
-                className={`${styles.line} ${styles.cornerLineRight}`}
-                src={cornerLineRight}
-                alt="Декоративный элемент"
-              />
+      <div className={styles.mainPageContainer}>
+        <Header />
+        <main className={styles.main}>
+          <section className={styles.introduction}>
+            <div className={styles.blockWithLink}>
+              <h1 className={styles.heading}>GeoAnalytics.ai</h1>
+              <p className={styles.caption}>
+                Платформа для эффективного управления территорией на основе
+                искусственного интеллекта
+              </p>
+              <Link to="/application">
+                <Button content="Демо-доступ" />
+              </Link>
             </div>
-          </div>
-        </section>
-      </main>
-      <Footer />
-      {/* <img className="earth" src={earth} alt="планета Земля" /> */}
+          </section>
+          <section className={styles.keyOpportunities}>
+            <div className={styles.keyOpportunitiesContainer}>
+              <h2 className={styles.keyOpportunitiesTitle}>
+                Ключевые возможности
+              </h2>
+              <div className={styles.cardsContainer}>
+                {keyOpportunityCards.map((card) => {
+                  return (
+                    <OpportunityCard
+                      image={card.image}
+                      imageCaption={card.title}
+                      title={card.title}
+                      content={card.caption}
+                    />
+                  );
+                })}
+                <img
+                  className={`${styles.line} ${styles.cornerLineLeft}`}
+                  src={cornerLineLeft}
+                  alt="Декоративный элемент"
+                />
+                <img
+                  className={`${styles.line} ${styles.straightLine}`}
+                  src={straightLine}
+                  alt="Декоративный элемент"
+                />
+                <img
+                  className={`${styles.line} ${styles.cornerLineRight}`}
+                  src={cornerLineRight}
+                  alt="Декоративный элемент"
+                />
+              </div>
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
